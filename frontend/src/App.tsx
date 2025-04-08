b72f504f8f4c14c480932cb50e4d05abf506806e
Ki3mON <maciej.kmak@op.pl>
2025-04-08 22:07:05 +0200

    map work

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { MapContainer, TileLayer } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <div className="map-wrapper">
+            <MapContainer
+                center={[52.2297, 21.0122]}
+                zoom={6}
+                scrollWheelZoom={true}
+                style={{ height: '100vh', width: '100%' }}
+            >
+                {/* Warstwa bazowa: DarkMatter */}
+                <TileLayer
+                    url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+                    maxZoom={20}
+                    minZoom={0}
+                    attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                />
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more LMAO
-      </p>
-    </>
-  )
+                {/* Warstwa szlaków: Waymarked Trails */}
+                <TileLayer
+                    url="https://tile.waymarkedtrails.org/hiking/{z}/{x}/{y}.png"
+                    maxZoom={18}
+                    attribution= 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | Map style: &copy; <a href="https://waymarkedtrails.org">waymarkedtrails.org</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+                />
+            </MapContainer>
+        </div>
+    )
 }
 
 export default App
