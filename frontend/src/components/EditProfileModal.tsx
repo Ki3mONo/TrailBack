396b30de7679d379cbd0b8867f72a4ac360bffeb
Ki3mON <maciej.kmak@op.pl>
2025-04-19 17:53:29 +0200

    fixing editing profile

--- frontend/src/components/EditProfileModal.tsx
+++ frontend/src/components/EditProfileModal.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import axios from "axios";
 
-const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:8000";
+const API_BASE = import.meta.env.VITE_BACKEND_URL;
 
 type Props = {
     userId: string;
