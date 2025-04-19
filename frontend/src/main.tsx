1beb95b2cd21b25479c7a8a658c9b41a1d6d454d
Ki3mON <maciej.kmak@op.pl>
2025-04-19 11:44:16 +0200

    Configures Leaflet marker icons for improved map rendering

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-// import L from "leaflet";
 import 'leaflet/dist/leaflet.css';
 import './index.css';
+import L from 'leaflet';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: markerIcon2x,
+    iconUrl: markerIcon,
+    shadowUrl: markerShadow,
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
